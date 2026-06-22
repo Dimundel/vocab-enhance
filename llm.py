31e7dccec8b6c440a7e8bbcf72a4a515a8b7fd37
Dmitrii <deenixie@gmail.com>
2026-06-22 18:13:47 +0300

    feat: add llm.py file

---
+++ llm.py
@@ -0,0 +1,18 @@
+import json
+from pydantic import BaseModel
+from config import CLIENT, EXTRACT_PROMPT
+
+
+class VocabularyWord(BaseModel):
+    word: str
+    definition: str
+    context: str
+    simple_synonym: str
+
+
+def extract_words_from_text(text: str) -> list[dict]:
+    prompt = EXTRACT_PROMPT + "\n" + text
+    response = CLIENT.models.generate_content(
+        model="gemini-flash-lite-latest", contents=prompt
+    )
+    return json.loads(response)
